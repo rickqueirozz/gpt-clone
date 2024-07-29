d12c8a5914b5a101bae5b0cc091af7d8ea6d7446
Ricardo <rick-queiroz@live.com>
2024-07-29 17:25:23 -0300

    feat:Cria o componente SideBar e adiciona o upperSide

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
+import SideBar from './components/SideBar';
 
 const App = () => {
   return (
     <div className='app'>
-      
+      <SideBar/>
     </div>
   )
 }
