56b825e990f6f331fa3ffc2989eb545c488b5995
Ricardo <rick-queiroz@live.com>
2024-07-29 18:00:42 -0300

    feat(SideBar):Adiciona a logo

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './SideBar.css'
+import chatgpt from '../assets/chatgpt.svg'
 
 
 const SideBar = () => {
   return (
-    <div className='sidebar'>
+    <div className='sideBar'>
       <div className="upperSide">
-        <div className="upperSideTop"><img src="" alt="" className='logo'/><span>ChatGPT</span> </div>
+        <div className="upperSideTop"><img src={chatgpt} alt="" className='logo'/><span>ChatGPT</span> </div>
           <button className='midBtn'><img src="" alt="" className="addBtn" />New Chat</button>
           <div className="upperSideBottom">
             <button className="query"><img src="" alt="" />What is Programming ?</button>
